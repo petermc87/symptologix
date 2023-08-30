37781ad1badbca98a5047ecfe8b5ba4b2b4124b6
petermc87 <peter.mcgibney@gmail.com>
2023-08-30 11:38:19 -0400

    feat: add prisma, knex and postgres

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: '.env.development'})
+import type { Knex } from "knex";
+// Update with your config settings.
+
+const config: Knex.Config = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    extension: 'ts'
+  }
+}
+
+module.exports = config;
