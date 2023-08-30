9ef71f1d98a8089c053c2b88b61481be0d887a14
petermc87 <peter.mcgibney@gmail.com>
2023-08-30 12:28:54 -0400

    feat: add page to read and generate posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import db from './modules/db'
+import { faker } from '@faker-js/faker'
+import { revalidatePath } from 'next/cache'
+import Button from './components/Button'
 
-export default function Home() {
+export default async function Home() {
+  // Fetching data from the database.
+  const posts = await db.post.findMany({ orderBy: {createdAt: 'desc'}})
+
+  //  Server side function -> creates the post request to the database.
+  const generatePosts = async () => {
+    'use server'
+
+    await db.post.createMany({
+      data: [
+        { content: faker.lorem.sentence() },
+        { content: faker.lorem.sentence() },
+        { content: faker.lorem.sentence() },
+      ]
+    })
+    revalidatePath('/')
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button onClick={generatePosts}>Generate Posts</Button>
+      {posts.map(post => (
+        <div key={post.id}>{post.content}</div>
+      ))}
     </main>
   )
 }
