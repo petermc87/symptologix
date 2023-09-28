340432377c893f5faea97200fe05f1b035f4604e
petermc87 <peter.mcgibney@gmail.com>
2023-09-27 22:58:14 -0400

    style: metrics container

--- src/app/components/Perks/Perks.tsx
+++ src/app/components/Perks/Perks.tsx
@@ -8,15 +8,23 @@ export default function Perks() {
           Log your symptoms with ease
         </h1>
         <div className={styles.chartContainer}>
-          <div className={styles.inputField}>INPUT</div>
-          <div className={styles.currentLog}>LOG</div>
+          <div id={styles.inputField} className={styles.chartType}>
+            INPUT
+          </div>
+          <div id={styles.currentLog} className={styles.chartType}>
+            LOG
+          </div>
         </div>
       </div>
       <div className={styles.perkContainer} id={styles.metrics}>
         <h1 className={styles.headingText}>Look at metrics over time</h1>
         <div className={styles.chartContainer}>
-          <div className={styles.pie}>PIE</div>
-          <div className={styles.bar}>BAR</div>
+          <div id={styles.pie} className={styles.chartType}>
+            PIE
+          </div>
+          <div id={styles.bar} className={styles.chartType}>
+            BAR
+          </div>
         </div>
       </div>
       <div className={styles.perkContainer}>
