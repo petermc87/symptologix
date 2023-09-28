a8d2e9fea5f3e1b1b8583a1f834417a1ab0b06bb
petermc87 <peter.mcgibney@gmail.com>
2023-09-28 13:51:39 -0400

    feat: add chart.js doughnut chart to landing page

--- src/app/components/Perks/Perks.tsx
+++ src/app/components/Perks/Perks.tsx
@@ -1,6 +1,31 @@
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 import styles from "./Perks.module.scss";
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 export default function Perks() {
+  // Dataset for doughnut chart.
+  const data = {
+    labels: ["Light", "Medium", "Dark"],
+    datasets: [
+      {
+        label: "# of occorances",
+        data: [20, 30, 5],
+        backgroundColor: [
+          "rgba(147, 145, 255, 0.9)",
+          "rgba(105, 102, 212, 0.9)",
+          "rgba(0, 7, 89, 0.9)",
+        ],
+        borderColor: [
+          "rgba(147, 145, 255, 1)",
+          "rgba(105, 102, 212, 1)",
+          "rgba(0, 7, 89, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <>
       <div className={styles.perkContainer}>
@@ -19,11 +44,12 @@ export default function Perks() {
       <div className={styles.perkContainer} id={styles.metrics}>
         <h1 className={styles.headingText}>Look at metrics over time</h1>
         <div className={styles.chartContainer}>
-          <div id={styles.pie} className={styles.chartType}>
+          {/* <div id={styles.pie} className={styles.chartType}>
             PIE
-          </div>
+          </div> */}
+          <div className={styles.chartType}>BAR</div>
           <div id={styles.bar} className={styles.chartType}>
-            BAR
+            <Doughnut data={data} />
           </div>
         </div>
       </div>
