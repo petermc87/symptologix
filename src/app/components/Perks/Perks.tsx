63771eef3384514dde79881cc773c0f6de1cb96d
petermc87 <peter.mcgibney@gmail.com>
2023-09-28 23:02:25 -0400

    style: add metrics visuals

--- src/app/components/Perks/Perks.tsx
+++ src/app/components/Perks/Perks.tsx
@@ -1,21 +1,90 @@
-import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 import styles from "./Perks.module.scss";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  ArcElement,
+  Tooltip,
+  Legend
+);
+
+// Changing the color of the doughnut default text.
+Chart.overrides.doughnut.color = "#000";
+Chart.overrides.bar.color = "#000";
+Chart.defaults.color = "#000";
+// Chart.defaults.borderColor = "rgba(239, 239, 240, 1)";
+// Chart.overrides.line.borderColor = "rgba(239, 239, 240, 1)";
+Chart.overrides.bar.borderColor = "rgba(239, 239, 240, 1)";
+// Chart.defaults.bor = "0.4";
 
 export default function Perks() {
+  // Options for line chart.
+  const options = {
+    responsiveness: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: " Symptoms over time",
+      },
+    },
+  };
+  const labels = ["January", "February", "March", "April", "May"];
+
+  // Dataset for line chart.
+  const lineData = {
+    labels,
+
+    datasets: [
+      {
+        label: "Smell",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        borderColor: "rgba(0, 7, 89)",
+        backgroundColor: "rgba(0, 7, 89, 0.5)",
+        borderWidth: 2,
+      },
+      {
+        label: "Taste",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        borderColor: "rgba(99, 93, 255)",
+        backgroundColor: "rgba(99, 93, 255, 0.5)",
+        borderWidth: 2,
+      },
+    ],
+  };
+
   // Dataset for doughnut chart.
   const data = {
-    labels: ["Light", "Medium", "Dark"],
+    labels: ["Smell", "Taste", "Emotion"],
     datasets: [
       {
         label: "# of occorances",
         data: [20, 30, 5],
         backgroundColor: [
-          "rgba(147, 145, 255, 0.9)",
-          "rgba(105, 102, 212, 0.9)",
-          "rgba(0, 7, 89, 0.9)",
+          "rgba(147, 145, 255, 1)",
+          "rgba(105, 102, 212, 1)",
+          "rgba(0, 7, 89, 1)",
         ],
         borderColor: [
           "rgba(147, 145, 255, 1)",
@@ -23,6 +92,25 @@ export default function Perks() {
           "rgba(0, 7, 89, 1)",
         ],
         borderWidth: 1,
+        // Determines how big the hole in the middle of the chart is.
+        cutout: "40%",
+      },
+    ],
+  };
+
+  // Dataset for barchart
+  const barData = {
+    labels,
+    datasets: [
+      {
+        label: "Smell",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        backgroundColor: "rgba(0, 7, 89)",
+      },
+      {
+        label: "Taste",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        backgroundColor: "rgba(147, 145, 255, 1)",
       },
     ],
   };
@@ -42,27 +130,24 @@ export default function Perks() {
         </div>
       </div>
       <div className={styles.perkContainer} id={styles.metrics}>
-        <h1 className={styles.headingText}>Look at metrics over time</h1>
-        <div className={styles.chartContainer}>
-          {/* <div id={styles.pie} className={styles.chartType}>
-            PIE
-          </div> */}
-          <div className={styles.chartType}>BAR</div>
+        <h1 className={styles.headingText}>Pinpoint your issue</h1>
+        <div id={styles.issuesCharts} className={styles.chartContainer}>
           <div id={styles.bar} className={styles.chartType}>
+            <Bar options={options} data={barData} />
+          </div>
+          <div className={styles.chartType}>
             <Doughnut data={data} />
           </div>
         </div>
       </div>
       <div className={styles.perkContainer}>
         <h1 className={styles.headingText} id={styles.logsText}>
-          Pinpoint your issue
+          Look at metrics over time
         </h1>
+
         <div className={styles.chartContainer}>
-          <div id={styles.pie} className={styles.chartType}>
-            PIE
-          </div>
-          <div id={styles.bar} className={styles.chartType}>
-            BAR
+          <div id={styles.lineChart} className={styles.chartType}>
+            <Line options={options} data={lineData} />
           </div>
         </div>
       </div>
