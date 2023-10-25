b2e17b5f523748a91f5c21f9f1b99af7dd2f40dd
petermc87 <peter.mcgibney@gmail.com>
2023-10-25 01:21:06 -0400

    feat: add st timeout for logging out so it will reroute to landing

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import GeneralButton from "../Button/Button";
 import Hamburger from "../Hamburger/Hamburger";
 import Logo from "../Logo/Logo";
@@ -9,6 +10,10 @@ export default function NavBar() {
   // Check the user session data for a login object coming back.
   // If it does, then show the logout and other links
   const { data } = useSession();
+
+  // Instigate the useRouter hook to route back to the landing page
+  // when the logout button is selected.
+  const router = useRouter();
 
   return (
     <div className={styles.navbarWrapper}>
@@ -32,7 +37,17 @@ export default function NavBar() {
           <Link href={"/insights"} className={styles.desktopLinks}>
             <div>Insights</div>
           </Link>
-          <Link href={"/"} className={styles.desktopLinks}>
+          <Link
+            href={"/"}
+            className={styles.desktopLinks}
+            onClick={() => {
+              console.log("click before");
+              setTimeout(() => {
+                console.log("click");
+                router.push("/");
+              }, 4000);
+            }}
+          >
             <GeneralButton name="Log Out" />
           </Link>
         </>
