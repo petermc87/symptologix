5c289408ed275f684d853cde7fc475f2dd788cc8
petermc87 <peter.mcgibney@gmail.com>
2023-11-28 12:53:59 -0500

    style: add heading text

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+import SolidLine from "../components/SolidLine/SolidLine";
+import styles from "./page.module.scss";
+
 export default function Settings() {
-  return <div>Settings</div>;
+  return (
+    <>
+      <NavBar />
+      <div className={styles.pageContainer}>
+        <div className={styles.headingText} id={styles.reminder}>
+          Set Your Reminders
+        </div>
+        <SolidLine />
+        <div className={styles.headingText} id={styles.profile}>
+          Your Profile
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
