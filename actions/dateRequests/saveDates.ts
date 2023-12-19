bf31f8e2c7b55dd5c8e04854031da2a3a9902bcd
petermc87 <peter.mcgibney@gmail.com>
2023-12-19 13:19:44 -0500

    feat: calendar updates working

--- actions/dateRequests/saveDates.ts
+++ actions/dateRequests/saveDates.ts
@@ -3,11 +3,31 @@
 import db from "@/app/modules/db";
 
 export default async function saveDates(to: Date, from: Date, id: string) {
-  await db.calendarDates.create({
-    data: {
-      fromDate: from,
-      toDate: to,
+  // Check if there is a date object already (there should only be one).
+  const currentDates = await db.calendarDates.findFirst({
+    where: {
       user: id,
     },
   });
+  // If it does exist, update with the new data.
+  if (currentDates) {
+    await db.calendarDates.update({
+      where: {
+        id: currentDates.id,
+      },
+      data: {
+        fromDate: from,
+        toDate: to,
+      },
+    });
+    // Otherwise, create a new record.
+  } else {
+    await db.calendarDates.create({
+      data: {
+        fromDate: from,
+        toDate: to,
+        user: id,
+      },
+    });
+  }
 }
